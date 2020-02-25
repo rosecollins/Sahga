2e9935cc85ff58934906651707174321b26fe621
rosecollins <45430622+rosecollins@users.noreply.github.com>
2020-02-25 15:24:32 +0300

    Update sahgat.blade.php

--- resources/views/sahgat.blade.php
+++ resources/views/sahgat.blade.php
@@ -12,7 +12,7 @@
                     <div class="card-content white-text">
                         <h3><b> {{ $sahgat->sahga }} </b></h3>
                         <h5> <b> {{ $sahgat->name }} </b>  ساحگـ\ـه وگايلـ\ـه <b> {{ $sahgat->sahga }} </b> بدل ما تـ\يـگول <b> {{ $sahgat->nosahga }} </b>  </h5>
-                        <h6> {{ $sahgat->created_at }} </h6>
+                        <h6> {{ $sahgat->created_at->diffForHumans() }} </h6>
                     </div>
                 </div>
             </div>
