5531ab60e7dcb201245af52f44287245b29ee636
rosecollins <45430622+rosecollins@users.noreply.github.com>
2020-02-25 15:23:12 +0300

    Updating the look of the Sahgat cards.

--- resources/views/sahgat.blade.php
+++ resources/views/sahgat.blade.php
@@ -5,18 +5,19 @@
     <?php if($count<1){
             echo "ماكو سحگات حمبي xD";
     }?>
-    <?php foreach($sahgas as $sahgat) {?>
     <div class="row">
-        <div class="col s12 m12">
-            <div class="card lo">
-                <div class="card-content white-text">
-                    <h3><b> {{ $sahgat->sahga }} </b></h3>
-                    <h5> <b> {{ $sahgat->name }} </b>  ساحگـ\ـه وگايلـ\ـه <b> {{ $sahgat->sahga }} </b> بدل ما تـ\يـگول <b> {{ $sahgat->nosahga }} </b>  </h5>
-                    <h6> {{ $sahgat->created_at }} </h6>
+        <?php foreach($sahgas as $sahgat) {?>
+            <div class="col s12 m6 lg4">
+                <div class="card lo">
+                    <div class="card-content white-text">
+                        <h3><b> {{ $sahgat->sahga }} </b></h3>
+                        <h5> <b> {{ $sahgat->name }} </b>  ساحگـ\ـه وگايلـ\ـه <b> {{ $sahgat->sahga }} </b> بدل ما تـ\يـگول <b> {{ $sahgat->nosahga }} </b>  </h5>
+                        <h6> {{ $sahgat->created_at }} </h6>
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php } ?>
     </div>
-    <?php } ?>
+    
 
 @endsection
